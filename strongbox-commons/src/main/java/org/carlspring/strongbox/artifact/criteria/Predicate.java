13cbd8c26d16e6317515e353a2c1d2b3f4692797
sbespalov <serge.bespalov@gmail.com>
2018-02-08 19:00:51 +0600

    SB-1004: ArtifactEntry Criteria API

---
+++ strongbox-commons/src/main/java/org/carlspring/strongbox/artifact/criteria/Predicate.java
@@ -0,0 +1,67 @@
+package org.carlspring.strongbox.artifact.criteria;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.util.Assert;
+
+public class Predicate
+{
+
+    private ArtefactEntryCriteria criteria;
+
+    private BooleanOperator operator = BooleanOperator.AND;
+
+    private List<Predicate> predicate = new ArrayList<>();
+
+    public Predicate()
+    {
+        super();
+    }
+
+    public ArtefactEntryCriteria getCriteria()
+    {
+        return criteria;
+    }
+
+    public BooleanOperator getOperator()
+    {
+        return operator;
+    }
+
+    public List<Predicate> getPredicate()
+    {
+        return predicate;
+    }
+
+    public Predicate eq(ArtefactEntryCriteria c)
+    {
+        this.criteria = c;
+        return this;
+    }
+
+    public Predicate or(Predicate p)
+    {
+        Assert.state(!BooleanOperator.AND.equals(this.operator), "Only disjunction allowed.");
+
+        this.operator = BooleanOperator.OR;
+        this.predicate.add(p);
+
+        return this;
+    }
+
+    public Predicate and(Predicate p)
+    {
+        Assert.state(!BooleanOperator.OR.equals(this.operator), "Only conjunction allowed.");
+
+        this.operator = BooleanOperator.OR;
+        this.predicate.add(p);
+
+        return this;
+    }
+
+    public static enum BooleanOperator
+    {
+        AND, OR
+    }
+}
---
+++ strongbox-commons/src/main/java/org/carlspring/strongbox/artifact/criteria/Selector.java
@@ -0,0 +1,49 @@
+package org.carlspring.strongbox.artifact.criteria;
+
+public class Selector
+{
+
+    private Projection projection = Projection.ROWS;
+
+    private Predicate predicate;
+
+    public Predicate getPredicate()
+    {
+        return predicate;
+    }
+
+    public Projection getProjection()
+    {
+        return projection;
+    }
+
+    public Selector select(Projection p)
+    {
+        this.projection = p;
+        return this;
+    }
+
+    public Selector where(ArtefactEntryCriteria c)
+    {
+        this.predicate = new Predicate().eq(c);
+        return this;
+    }
+
+    public Predicate eq(ArtefactEntryCriteria c)
+    {
+        return new Predicate().eq(c);
+    }
+
+    public Predicate and(Predicate p1,
+                                      Predicate p2)
+    {
+        return p1.and(p2);
+    }
+
+    public Predicate or(Predicate p1,
+                                     Predicate p2)
+    {
+        return p1.or(p2);
+    }
+
+}
